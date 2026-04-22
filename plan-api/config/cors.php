b3c280e012d0977a386f9404c226e933f7584681
Soulayman Elkharraz <soulaymanelkharraz2006@gmail.com>
2026-04-22 21:48:26 +0100

    first commit

--- plan-api/config/cors.php
+++ plan-api/config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'https://taskzero.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
